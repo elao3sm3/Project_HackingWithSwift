8f5d653978692d48947a6c15fb6f7c2a29ed24c3
Maimin46 <elao3sm3@gmail.com>
2017-12-19 15:00:49 +0800

    Project4_complete

--- Project_HackingWithSwift/Project4/Project4_HomePage.swift
+++ Project_HackingWithSwift/Project4/Project4_HomePage.swift
@@ -13,6 +13,8 @@ class Project4_HomePage: UIViewController {
 
     // MARK: - Properties
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     
     // MARK: - IBOutlet
     
@@ -28,19 +30,82 @@ class Project4_HomePage: UIViewController {
         super.viewDidLoad()
         
         self.navigationItem.title = "Project4_EasyBrowser"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+//        let url = URL(string: "https://www.hackingwithswift.com")!
+        // 重構
+        let url = URL(string: "https://" + websites[0])!
+        
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+       
     }
     
     // MARK: - IBAction
     
     // MARK: - Public
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
     
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+//        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+//        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+//      重構
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+        
+        //title = "\(action.title!)"
+    }
     // MARK: - Private
 
 }
 extension Project4_HomePage: WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
     
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        // 這邊檢查是不是還在認可的網站內,比如進入apple的產品頁面,依舊屬於apple/productshop 檢查前面依舊是apple
+        if let host = url!.host {
+            for website in websites {
+                if host.range(of: website) != nil {
+                    decisionHandler(.allow)
+                    print("\(webView.url!)")
+                    print("\(host)")
+                    print("\(websites)")
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
 }
